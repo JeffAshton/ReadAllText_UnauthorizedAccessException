2888311c8d637dfda86c2fa6e2567b551589e598
Jeff Ashton <Jeff.Ashton@D2L.com>
2020-01-10 15:32:59 -0500

    2 apps to demonstrate UnauthorizedAccessException when reading file in one process, while other process is moving/deleting the same file.

---
+++ FileWatcherMover/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace FileWatcherMover {
+
+	internal static class Program {
+
+		internal static int Main() {
+
+			string directory = Path.GetFullPath( "." );
+
+			string sourcePath = Path.Combine( directory, "source.txt" );
+			string configPath = Path.Combine( directory, "current.txt" );
+			string previousPath = Path.Combine( directory, "previous.txt" );
+
+			if( !File.Exists( sourcePath ) ) {
+
+				Console.Error.WriteLine( "Source file not found: {0}", sourcePath );
+				return 1;
+			}
+			
+			File.Delete( previousPath );
+
+			for(; ; ) {
+
+				string tempPath = Path.GetTempFileName();
+				File.Copy( sourcePath, tempPath, overwrite: true );
+
+				retryPreviousMove:
+				try {
+					File.Move( configPath, previousPath );
+
+				} catch( FileNotFoundException ) {
+
+				} catch( IOException err ) when( IsFileLocked( err ) ) {
+					goto retryPreviousMove;
+
+				} catch( Exception err ) {
+					Console.Error.WriteLine( "Failed to move current to previous: {0}", err );
+				}
+
+				retryConfigMove:
+				try {
+					File.Move( tempPath, configPath );
+
+				} catch( IOException err ) when( IsFileLocked( err ) ) {
+					goto retryConfigMove;
+
+				} catch( Exception err ) {
+					Console.Error.WriteLine( "Failed to move new current: {0}", err );
+				}
+
+				retryDelete:
+				try {
+					File.Delete( previousPath );
+
+				} catch( IOException err ) when( IsFileLocked( err ) ) {
+					goto retryDelete;
+
+				} catch( Exception err ) {
+					Console.Error.WriteLine( "Failed to delete previous: {0}", err );
+				}
+
+				Thread.Sleep( 1000 );
+			}
+		}
+
+
+		private static bool IsFileLocked( IOException exception ) {
+			// We cannot guarantee that these HResult values will be present on non-Windows OSes. However, this
+			// logic is less important on other platforms, because in Unix-like OSes you can atomically replace a
+			// file's contents (by creating a temporary file and then renaming it to overwrite the original file),
+			// so FileDataSource will not try to read an incomplete update; that is not possibble in Windows.
+			int errorCode = exception.HResult & 0xffff;
+			switch( errorCode ) {
+				case 0x20: // ERROR_SHARING_VIOLATION
+				case 0x21: // ERROR_LOCK_VIOLATION
+					return true;
+				default:
+					return false;
+			}
+		}
+	}
+}
---
+++ FileWatcherReader/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileWatcherReader {
+
+	internal static class Program {
+
+		internal static void Main() {
+
+			string directory = Path.GetFullPath( "." );
+			Directory.CreateDirectory( directory );
+
+			string configPath = Path.Combine( directory, "current.txt" );
+			string previousPath = Path.Combine( directory, "previous.txt" );
+
+			SortedSet<string> paths = new SortedSet<string>( StringComparer.OrdinalIgnoreCase ) {
+				previousPath,
+				configPath
+			};
+
+			using( FileSystemWatcher fsw = new FileSystemWatcher( directory ) ) {
+
+				fsw.Created += delegate ( object sender, FileSystemEventArgs e ) {
+					if( paths.Contains( e.FullPath ) ) {
+						ReadFile( paths );
+					}
+				};
+				fsw.Changed += delegate ( object sender, FileSystemEventArgs e ) {
+					if( paths.Contains( e.FullPath ) ) {
+						ReadFile( paths );
+					}
+				};
+				fsw.Renamed += delegate ( object sender, RenamedEventArgs e ) {
+					if( paths.Contains( e.FullPath ) ) {
+						ReadFile( paths );
+					}
+				};
+
+				fsw.EnableRaisingEvents = true;
+
+				Console.ReadLine();
+			}
+		}
+
+		private static void ReadFile( IEnumerable<string> paths ) {
+
+			foreach( string path in paths ) {
+
+				try {
+					FlagFileReader.ReadAllText( path );
+
+				} catch( FileNotFoundException ) {
+
+				} catch( Exception e ) {
+
+					Console.Error.WriteLine( e );
+					return;
+				}
+			}
+		}
+	}
+}
